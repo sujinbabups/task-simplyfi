1adbcf3b0b0ae7ee210a00a35eebe2f7cdca5cd7
sujinbabups <sujinbabu85@gmail.com>
2025-02-15 22:28:52 +0530

    profiles

--- Client/profile.js
+++ Client/profile.js
@@ -1,22 +1,5 @@
 let profile = {
-    organization1: {
-        "cryptoPath": "../Network/organizations/peerOrganizations/organization1.sample.com",
-        "keyDirectoryPath": "../Network/organizations/peerOrganizations/organization1.sample.com/users/User1@organization1.sample.com/msp/keystore/",
-        "certPath": "../Network/organizations/peerOrganizations/organization1.sample.com/users/User1@organization1.sample.com/msp/signcerts/cert.pem",
-        "tlsCertPath": "../Network/organizations/peerOrganizations/organization1.sample.com/peers/peer0.organization1.sample.com/tls/ca.crt",
-        "peerEndpoint": "localhost:7051",
-        "peerHostAlias": "peer0.organization1.sample.com",
-        "mspId": "organization1-sample-com"
-    },
-    organization2: {
-        "cryptoPath": "../Network/organizations/peerOrganizations/organization2.sample.com",
-        "keyDirectoryPath": "../Network/organizations/peerOrganizations/organization2.sample.com/users/User1@organization2.sample.com/msp/keystore/",
-        "certPath": "../Network/organizations/peerOrganizations/organization2.sample.com/users/User1@organization2.sample.com/msp/signcerts/cert.pem",
-        "tlsCertPath": "../Network/organizations/peerOrganizations/organization2.sample.com/peers/peer0.organization2.sample.com/tls/ca.crt",
-        "peerEndpoint": "localhost:9051",
-        "peerHostAlias": "peer0.organization2.sample.com",
-        "mspId": "organization2-sample-com"
-    },
+   
     organization1admin: { 
         "cryptoPath": "../Network/organizations/peerOrganizations/organization1.sample.com",
         "keyDirectoryPath": "../Network/organizations/peerOrganizations/organization1.sample.com/users/Admin@organization1.sample.com/msp/keystore/",
